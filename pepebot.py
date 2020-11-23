7d9dbdfad37b8926e29de5e0c4f700b505f40ff2
facub <bucfacundo@gmail.com>
2020-11-23 19:59:35 -0300

    Update pepebot.py

--- pepebot.py
+++ pepebot.py
@@ -1,11 +1,16 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix='=')
+
+@client.command(name='puto')
+async def version(context):
+    await context.message.channel.send('el que lee')
+
 
 @client.event
 async def on_ready():
     general_channel = client.get_channel(780505808961273884)
-
     await general_channel.send('hi')
 
 
