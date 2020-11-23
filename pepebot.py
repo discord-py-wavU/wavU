c8b99b231241bce44d1b27114ac6a614ce54de0d
facub <bucfacundo@gmail.com>
2020-11-23 20:35:51 -0300

    Update pepebot.py

--- pepebot.py
+++ pepebot.py
@@ -7,11 +7,18 @@ client = commands.Bot(command_prefix='=')
 async def version(context):
     await context.message.channel.send('el que lee')
 
-
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('Minecraft'))
     general_channel = client.get_channel(780505808961273884)
     await general_channel.send('hi')
 
+@client.event
+async def on_message(message):
+    if message.content == 'eh vo':
+        await message.author.send('que te pasa gil?')
+
+    await client.process_commands(message)
 
 client.run('Mzc5MTU4Nzc0MTU5NTA3NDU5.WgftIA.n-WrY7dg3ZvJzfVcE-IKldH-Hww')
+
