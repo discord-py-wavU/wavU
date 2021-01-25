b45cf38bae278d1cdc213b3ea90fbaaadb8f8c73
facub <bucfacundo@gmail.com>
2021-01-25 11:29:26 -0300

    folder server name added

--- cogs/download.py
+++ cogs/download.py
@@ -2,30 +2,40 @@ import discord
 from discord.ext import commands
 import os
 import requests
+import config
 
 class DownloadFile(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.attachments:
-            headers = {
-            'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
-            }
+    @commands.command()
+    async def add(self, ctx):
 
-            r = requests.get(message.attachments[0].url, headers=headers, stream=True)
-            mp3 = message.attachments[0].filename.split('.')
-            if mp3[len(mp3)-1] == "mp3":
-                with open('audio/' + message.attachments[0].filename, 'wb') as f:
-                    for chunk in r.iter_content():
-                        if chunk:
-                            f.write(chunk)
-                f.close()
+        await ctx.send("Update a .mp3 file")
+        
+        newpath = config.path + "/" + ctx.message.guild.name
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
 
+        def check(m):
+            return m.attachments
+        msg = await self.client.wait_for('message', check=check)
 
+        headers = {
+        'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
+        }
 
+        r = requests.get(msg.attachments[0].url, headers=headers, stream=True)
+        mp3 = msg.attachments[0].filename.split('.')
+        if mp3[len(mp3)-1] == "mp3":
+            with open('audio/' + ctx.message.guild.name + '/' + msg.attachments[0].filename, 'wb') as f:
+                for chunk in r.iter_content():
+                    if chunk:
+                        f.write(chunk)
+            f.close()
+        else:
+            await ctx.send("This is not a .mp3 file")
 
 def setup(client):
     client.add_cog(DownloadFile(client))
--- cogs/voice_commands.py
+++ cogs/voice_commands.py
@@ -18,7 +18,7 @@ class VoiceCommands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        path = config.path
+        path = config.path + '/' + member.guild.name
         if after.channel is not None and before.channel is not member.voice.channel and member != self.client.user:
             try:
                 voice = get(self.client.voice_clients, guild=member.guild)
@@ -27,33 +27,15 @@ class VoiceCommands(commands.Cog):
                 else:
                     voice = await after.channel.connect()
                 onlyfiles = [f for f in listdir(path) if isfile(join(path, f)) and '.mp3' in f]
-                #print(get_current_time() + ' => ' + member.guild.name + ' :: '
-                #      + member.display_name + ' -> ' + after.channel.name)
                 audios_to_play = onlyfiles
                 time.sleep(1)
-                #print(self.client.voice_clients)
-                audio_to_play = 'audio/' + random.choice(audios_to_play)
+                audio_to_play = 'audio/' + member.guild.name + '/' + random.choice(audios_to_play)
                 voice.play(discord.FFmpegPCMAudio(audio_to_play))
                 while voice.is_playing():
                     time.sleep(0.3)
                 voice.stop()
                 if voice.is_connected():
-                    print(voice.latency)
-                    # while voice.latency == float('inf'):
-                    # print(voice.latency)
-                    # print(voice.average_latency)
                     await voice.disconnect()
-
-                if after.channel.name == 'General' or after.channel.name == 'aaaaa':
-                    print('User is in channel')
-                    print(member.voice)
-                    #print(member.voice_clients)
-                    #await member.disconnect()
-
-                    # await member.voice.disconnect()
-                    # voice2 = get(member.voice.channel, guild=member.guild)
-                    # print(voice2)
-                    # await voice.disconnect()
 
             except discord.ClientException:
                 print("Error")
