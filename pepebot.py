9115dc90648f8c39f64e1411be2a1d67d5d3d9c1
facub <bucfacundo@gmail.com>
2021-01-26 19:54:59 -0300

    Update pepebot.py

--- pepebot.py
+++ pepebot.py
@@ -12,27 +12,6 @@ client = commands.Bot(command_prefix=config.prefix)
 async def on_ready():
     await client.change_presence(status=config.status, activity=discord.Game(config.game))
 
-
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
-
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-
-
-@client.command()
-async def reload(ctx, extension):
-    try:
-        client.unload_extension(f'cogs.{extension}')
-    except discord.ext.commands.errors.ExtensionNotLoaded:
-        print('Extension already unloaded')
-    finally:
-        client.load_extension(f'cogs.{extension}')
-
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
