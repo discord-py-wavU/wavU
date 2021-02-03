6ea0d7bfd81bc8f62091d5d346e0033f01d13a34
facub <bucfacundo@gmail.com>
2021-02-03 00:26:51 -0300

    Auxiliary functions added

--- cogs/voice_commands.py
+++ cogs/voice_commands.py
@@ -18,64 +18,97 @@ class VoiceCommands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @staticmethod
+    def check_db(member):
+        servers = db.all_servers(False)
+        is_on = True
+        for server in servers:
+            if str(member.guild.name) == server[1] and server[2] == 0:
+                is_on = False
+
+        return is_on
+
+    @staticmethod
+    async def connect(voice, after):
+
+        if voice and voice.is_connected():
+            await voice.move_to(after.channel)
+        else:
+            voice = await after.channel.connect()
+
+        return voice
+
+    @staticmethod
+    async def search_song(self, path, member, after):
+
+        is_Empty = True
+        is_on = self.check_db(member)
+        audio_to_play = []
+        voice = get(self.client.voice_clients, guild=member.guild)
+
+        if is_on:
+            try:
+                member_path = path + '/' + str(member)
+                audios_to_play = [f for f in listdir(member_path) if isfile(join(member_path, f)) and '.mp3' in f]
+            except Exception as e:
+                print(str(e))
+                audios_to_play = []
+
+            if not audios_to_play:
+                try:
+                    audios_to_play = [f for f in listdir(path) if isfile(join(path, f)) and '.mp3' in f]
+                except Exception as e:
+                    print(str(e))
+                    audios_to_play = []
+                if audios_to_play:
+                    print("hi")
+                    is_Empty = False
+                    voice = await self.connect(voice, after)
+                    audio_to_play = 'audio/' + member.guild.name + '/' + random.choice(audios_to_play)
+            elif audios_to_play:
+                is_Empty = False
+                voice = await self.connect(voice, after)
+                audio_to_play = 'audio/' + member.guild.name + '/' \
+                                + str(member) + '/' + random.choice(audios_to_play)
+
+        return audio_to_play, voice, is_Empty
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         loop = self.client.loop or asyncio.get_event_loop()
         path = config.path + '/' + member.guild.name
         if after.channel is not None and before.channel is not member.voice.channel and member != self.client.user:
 
-            servers = db.all_servers(False)
+            audio_to_play, voice, is_Empty = await self.search_song(self, path, member, after)
 
-            for server in servers:
-                if str(member.guild.name) == server[1] and server[2] == 0:
-                    return
-
-            member_path = path + '/' + str(member)
-            try:
-                voice = get(self.client.voice_clients, guild=member.guild)
-                if voice and voice.is_connected():
-                    await voice.move_to(after.channel)
-                else:
-                    voice = await after.channel.connect()
+            if not is_Empty:
                 try:
-                    audios_to_play = [f for f in listdir(member_path) if isfile(join(member_path, f)) and '.mp3' in f]
-                except Exception as e:
-                    print(str(e))
-                    audios_to_play = []
-                if not audios_to_play:
-                    audios_to_play = [f for f in listdir(path) if isfile(join(path, f)) and '.mp3' in f]
-                    audio_to_play = 'audio/' + member.guild.name + '/' + random.choice(audios_to_play)
-                else:
-                    audio_to_play = 'audio/' + member.guild.name + '/' \
-                                    + str(member) + '/' + random.choice(audios_to_play)
-               
-                if not voice.is_playing():
-                    partial = functools.partial(voice.play, discord.FFmpegPCMAudio(audio_to_play))
-                    await loop.run_in_executor(None, partial)
-                    await asyncio.sleep(2)
-                else:
+                    if not voice.is_playing():
+                        partial = functools.partial(voice.play, discord.FFmpegPCMAudio(audio_to_play))
+                        await loop.run_in_executor(None, partial)
+                        await asyncio.sleep(2)
+                    else:
+                        while voice.is_playing():
+                            await asyncio.sleep(0.5)
+                        partial = functools.partial(voice.play, discord.FFmpegPCMAudio(audio_to_play))
+                        await loop.run_in_executor(None, partial)
+                        await asyncio.sleep(0.5)
+
                     while voice.is_playing():
-                        await asyncio.sleep(0.5)  
-                    partial = functools.partial(voice.play, discord.FFmpegPCMAudio(audio_to_play))
-                    await loop.run_in_executor(None, partial)
-                    await asyncio.sleep(0.5)
+                        await asyncio.sleep(1)
 
-                while voice.is_playing():
-                    await asyncio.sleep(1)
+                    if voice.is_connected() and not voice.is_playing():
+                        await voice.disconnect()
 
-                if voice.is_connected() and not voice.is_playing():
-                    await voice.disconnect()
-
-            except discord.ClientException as e:
-                print("Error: " + str(e))
+                except discord.ClientException as e:
+                    print("Error: " + str(e))
         elif after.channel is None and len(before.channel.members) == 1:
             voice = get(self.client.voice_clients)
             if voice and voice.is_connected():
                 await voice.disconnect()
 
-    @commands.command(aliases=['ch', 'c'], help='Play a chosen .mp3 file')
-    async def choose(self, ctx, arg=None):
-        loop = self.client.loop or asyncio.get_event_loop()
+    @staticmethod
+    async def search_songs(ctx, arg):
         if arg is not None:
             path = config.path + "/" + ctx.message.guild.name + '/' + str(ctx.message.mentions[0])
         else:
@@ -85,6 +118,14 @@ class VoiceCommands(commands.Cog):
         except Exception as e:
             print(str(e))
             songs = []
+
+        return songs
+
+    @commands.command(aliases=['ch', 'c'], help='Play a chosen .mp3 file')
+    async def choose(self, ctx, arg=None):
+        loop = self.client.loop or asyncio.get_event_loop()
+
+        songs = await self.search_songs(ctx, arg)
 
         if songs:
             list_songs = ""
@@ -109,7 +150,6 @@ class VoiceCommands(commands.Cog):
                                         + str(ctx.message.mentions[0]) + '/' + songs[int(msg.content)-1]
                     else:
                         audio_to_play = 'audio/' + ctx.message.guild.name + '/' + songs[int(msg.content)-1]
-
 
                     if not voice.is_playing():
                         partial = functools.partial(voice.play, discord.FFmpegPCMAudio(audio_to_play))
